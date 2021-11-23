736f7191b6b03e8796b8067944a50109688978ef
rayane hachem <rayanehchm@gmail.com>
2021-11-23 11:31:42 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/dashboard', to: 'pages#dashboard'
+  get '/dashboard', to: 'dashboard#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artworks, only: [:index, :show, :new, :create] do
-    resources :artwork_prices, only: [:new, :create, :show]
-    end
+    resources :artwork_prices, only: [:create]
+    resources :upvotes, only: [:create]
   end
+end
