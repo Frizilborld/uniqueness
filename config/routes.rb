ca97c46e878336a96acaadbe9bc35081c5719ad3
rayane hachem <rayanehchm@gmail.com>
2021-11-23 11:07:03 +0100

    routes to artworks and artworks prices

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  resources :artworks, only: [:index, :show, :new, :create] do
+    resources :artwork_prices, only: [:new, :create, :show]
+    end
+  end
