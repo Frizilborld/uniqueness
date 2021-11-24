da321020a1886c37cd52a2de5f6a4a417224cd46
Thomas Petrelli <petrelli.thomas@orange.fr>
2021-11-24 13:04:02 +0100

    ok

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,21 +1,29 @@
 class ArtworksController < ApplicationController
   def new
+    @artwork = Artwork.new
   end
 
   def create
-    @artwork = Artwork.new(params[:name][:description][:photo])
-    photo = params[:upload_image][:photo]
-    @artwork.call(photo)
-    uploaded_file = UploadFileToApi.call(photo)
-    @artwork.color_tags_api_file_id = uploaded_file["file_id"]
-      # - appel de l'API report
-      # - mettre a jour les attributs
-      #   number_of_pixel_in_image
-      #   width
-      #   height
-      #   colors
-    @artwork.save
+    @artwork = Artwork.new(artwork_params)
 
+    uploaded_file = UploadFileToApi.call(artwork_params[:photo])
+    raise
+    @artwork.color_tags_api_file_id = uploaded_file["file_id"]
+
+    report = GetColorTags.call
+    @artwork.number_of_pixel_in_image = report["process"]
+    @artwork.width = report["process"]
+    @artwork.height = report["process"]
+    @artwork.colors = report["process"]
+    raise
+    @artwork.save
+    redirect_to artwork_path(@artwork)
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:photo, :name, :description)
   end
 
 end
