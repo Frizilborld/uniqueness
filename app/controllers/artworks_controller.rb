ec5e59415d7c81cb471c562a0727a312c0c4e4d3
Thomas Petrelli <petrelli.thomas@orange.fr>
2021-11-24 11:35:16 +0100

    ok

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -3,21 +3,18 @@ class ArtworksController < ApplicationController
   end
 
   def create
-     # TODO: a bouger dans la methode create du artworks controller
-    # pseudo code :
-    # - artwork.new avec les params
-    # - appel de l'API upload image
-    # - mettre a jour l'attribut
-    #(file = params[:upload_image][:file]
-    #uploaded_file = UploadFileToApi.call(file))
-    #   artwork.color_tags_api_file_id = uploaded_file["file_id"]
-    # - appel de l'API report
-    # - mettre a jour les attributs
-    #   number_of_pixel_in_image
-    #   width
-    #   height
-    #   colors
-    # - save l'artwork
+    @artwork = Artwork.new(params[:name][:description][:photo])
+    photo = params[:upload_image][:photo]
+    @artwork.call(photo)
+    uploaded_file = UploadFileToApi.call(photo)
+    @artwork.color_tags_api_file_id = uploaded_file["file_id"]
+      # - appel de l'API report
+      # - mettre a jour les attributs
+      #   number_of_pixel_in_image
+      #   width
+      #   height
+      #   colors
+    @artwork.save
 
   end
 
