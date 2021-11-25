0df634cf88f2b95230c7d72ab4c2e8f1a4e9aa71
rayane hachem <rayanehchm@gmail.com>
2021-11-25 12:25:34 +0100

    controler vote unique par utilisateur fonctionneml

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -2,10 +2,13 @@ class UpvotesController < ApplicationController
   def create
     @artwork = Artwork.find(params[:artwork_id])
     @user = current_user
-    @upvote = Upvote.new
-    @upvote.artwork = @artwork
-    @upvote.user = @user
-    if @upvote.save!
+
+    @upvote = Upvote.find_or_initialize_by(
+      artwork: @artwork,
+      user: @user
+    )
+
+    if @upvote.save
       redirect_to request.referer
       flash[:success] = "Your upvote was done"
     else
