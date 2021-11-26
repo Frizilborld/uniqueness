a110f6c4ccfaf1e9d50915b9f54a55e420013dde
Thomas Petrelli <petrelli.thomas@orange.fr>
2021-11-26 16:21:44 +0100

    add image data to 13 artworkds in seeds, 7 are still missing

--- app/services/artworks/seeds_in_api.rb
+++ app/services/artworks/seeds_in_api.rb
@@ -1,19 +1,38 @@
-class SeedsInApi
-  @artworks = Artwork.all.where(number_of_pixel_in_image: nil)
-  @artworks.each do |artwork|
-    uploaded_file = UploadFileToApi.call(@artwork[file])
-    @artwork.color_tags_api_file_id = uploaded_file["file_id"]
+require 'open-uri'
 
-    report = GetColorTags.call(@artwork.color_tags_api_file_id)
+module Artworks
+  class SeedsInApi
+    def self.call(artworks = nil)
+# [54, 56, 42, 43, 44, 46]
+      artworks ||= Artwork.where(number_of_pixel_in_image: nil)
 
+      artworks.each do |artwork|
+        puts "artwork: #{artwork.id}"
 
-    @artwork.number_of_pixel_in_image = report["result"]["number_of_pixel_in_image"]
-    @artwork.width  = report["result"]["width"]
-    @artwork.height = report["result"]["height"]
-    @artwork.colors = report["result"]["colors"]
-    @artwork.save!
+        photo_data = URI.open(artwork.photo.service_url).read
 
-    score = Artworks::ComputeScore.call(@artwork)
-    return score
+        photo_file = Tempfile.new(['photo', '.jpg'], binmode: true)
+
+        puts photo_file.path
+
+        photo_file.write(photo_data)
+        photo_file.rewind
+
+        binding.pry
+
+        uploaded_file = UploadFileToApi.call(photo_file)
+        artwork.color_tags_api_file_id = uploaded_file["file_id"]
+
+        report = GetColorTags.call(artwork.color_tags_api_file_id)
+
+        artwork.number_of_pixel_in_image = report["result"]["number_of_pixel_in_image"]
+        artwork.width  = report["result"]["width"]
+        artwork.height = report["result"]["height"]
+        artwork.colors = report["result"]["colors"]
+        artwork.save!
+
+        Artworks::ComputeScore.call(artwork)
+      end
+    end
   end
 end
