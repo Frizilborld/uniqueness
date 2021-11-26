8a38cee739f6c32717ff206e15c3ac045b296bd6
rayane hachem <rayanehchm@gmail.com>
2021-11-26 12:25:32 +0100

    creation de la route dashboard + methode

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,14 @@ class PagesController < ApplicationController
   @artworks = Artwork.all
   end
 
+  def dashboard
+     @my_artworks = Artwork.where(user: current_user)
+  end
+
+   private
+
+  def list_params
+    params.require(:artwork).permit(:name, :artwork_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/dashboard', to: 'dashboard#show'
+  get '/dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artworks, only: [:index, :show, :new, :create] do
     collection do
