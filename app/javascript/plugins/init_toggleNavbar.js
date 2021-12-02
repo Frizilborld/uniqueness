c5eaa225e71d738065d3ebdec3d577a80652d99d
Max Ellis <uraniumax@gmail.com>
2021-12-02 13:51:08 +0100

    navbar changes

--- app/javascript/plugins/init_toggleNavbar.js
+++ app/javascript/plugins/init_toggleNavbar.js
@@ -1,6 +1,5 @@
 function toggleNavbar (){
   window.addEventListener("scroll", function (){
-    console.log('hello');
     let Navbar = document.getElementById('smallnavbar');
     if(this.window.scrollY > 60){
       Navbar.classList.add("navbar-mini");
